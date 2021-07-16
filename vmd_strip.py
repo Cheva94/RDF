72307ba9a61ae4c029ef54292a1482a2fce250ca
Nacho Cheva <ichevallier@unc.edu.ar>
2021-07-16 17:19:17 -0300

    más frames

--- vmd_strip.py
+++ vmd_strip.py
@@ -2,10 +2,9 @@
 
 import pandas as pd
 
-file_in = 'example31_Si-Pt_VMD_PBC-on.csv'
+file_in = 'largo_Si-Pt_VMD_PBC-off.dat'
 vmd = pd.read_csv(file_in, header = None, delim_whitespace = True).iloc[:,1].to_numpy()
-print(vmd)
 
-with open('on.dat', 'w') as f:
+with open('off.dat', 'w') as f:
     for row in range(len(vmd)):
         f.write(f'{vmd[row]} \n')
