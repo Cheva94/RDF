334bfc1eb5a749ba25f5b2ac7647dc5075a7ecd9
Cheva <nacho.cheva@gmail.com>
2022-06-14 20:40:00 +0200

    .

--- plot_pdf.py
+++ plot_pdf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.10
 
 '''
-    Description: Plots 3D RDF curves.
+    Description: Plots PDF curves.
     Written by: Ignacio J. Chevallier-Boutell.
     Dated: April, 2022.
 '''
@@ -10,7 +10,7 @@ import argparse
 from pandas import read_csv
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+# from scipy.signal import find_peaks
 from matplotlib.ticker import MultipleLocator
 
 def main():
@@ -25,36 +25,26 @@ def main():
 
         for file in L:
             data = read_csv(f'{file}').to_numpy()
-            name = file.split('.csv')[0].split('_', 1)[1]
-
-            # peaks, _ = find_peaks(data[:,1], height=0.6, distance=8)
-            peaks, _ = find_peaks(data[:,1], height=0.001, distance=11)
-            peaksx, peaksy = data[:,0][peaks], data[:,1][peaks]
-
-            # valls, _ = find_peaks(-data[:,1], distance=10)
-            # vallsx, vallsy = data[:,0][valls], data[:,1][valls]
+            name = file.split('.csv')[0].split('_')[1]
 
             fig, ax = plt.subplots()
-            ax.plot(data[:,0], data[:,1], label = name)
-            ax.plot(peaksx, peaksy + 0.001, lw = 0, marker=11, color='black')
-            for i in range(len(peaksx)):
-                ax.annotate(f'{peaksx[i]:.2f}', xy = (peaksx[i], peaksy[i] + 0.0018), fontsize=10, ha='center')
-            # ax.plot(vallsx, 0.96*vallsy, lw = 0, marker=10, color='red')
-            # for i in range(len(vallsx)):
-            #     ax.annotate(f'{vallsx[i]:.2f}', xy = (vallsx[i], 0.88*vallsy[i]), fontsize=10, ha='center', color='red')
-            ax.set_xlabel(f'Distance [A]')
-            ax.set_ylabel('RDF [3D]')
-            ax.legend()
+
+            ax.axhspan(0.125, 1.902, color='k', alpha=0.10, label = "Ridges")
+            ax.axhspan(7.449, 9.286, color='k', alpha=0.10)
+
+            disp = ax.scatter(data[:,0], data[:,1], c = data[:,2], cmap = 'summer_r', s=10, label = name)
+            fig.colorbar(disp)
+            ax.set_xlabel('x [A]')
+            ax.set_ylabel('y [A]')
+            ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1), ncol = 4)
 
             if (left != None) and (right != None):
                 ax.set_xlim(float(left), float(right))
             if (bottom != None) and (top != None):
                 ax.set_ylim(float(bottom), float(top))
 
-            # ax.xaxis.set_major_locator(MultipleLocator(1))
-            # ax.xaxis.set_minor_locator(MultipleLocator(0.2))
-            ax.xaxis.set_major_locator(MultipleLocator(2))
-            ax.xaxis.set_minor_locator(MultipleLocator(1))
+            ax.xaxis.set_major_locator(MultipleLocator(1))
+            ax.xaxis.set_minor_locator(MultipleLocator(0.2))
 
             plt.savefig(f"{file.split('.csv')[0]}.png")
             print(f"Image file: {file.split('.csv')[0]}.png")
@@ -63,14 +53,18 @@ def main():
 
         F = []
         fig, ax = plt.subplots()
+        
+        ax.axhspan(0.125, 1.902, color='k', alpha=0.10, label = "Ridges")
+        ax.axhspan(7.449, 9.286, color='k', alpha=0.10)
+
         for file in L:
             data = read_csv(f'{file}').to_numpy()
-            name = file.split('.csv')[0].split('_', 1)[1]
+            name = file.split('.csv')[0].split('_')[1]
             F.append(name)
 
-            ax.plot(data[:,0], data[:,1], label = name)
-            ax.set_xlabel(f'Distance [A]')
-            ax.set_ylabel('RDF [3D]')
+            ax.scatter(data[:,0], data[:,1], label = name, s = 10)
+            ax.set_xlabel('x [A]')
+            ax.set_ylabel('y [A]')
             ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1), ncol = 4)
 
             if (left != None) and (right != None):
@@ -78,20 +72,17 @@ def main():
             if (bottom != None) and (top != None):
                 ax.set_ylim(float(bottom), float(top))
 
-        # ax.xaxis.set_major_locator(MultipleLocator(1))
-        # ax.xaxis.set_minor_locator(MultipleLocator(0.2))
-        ax.xaxis.set_major_locator(MultipleLocator(2))
-        ax.xaxis.set_minor_locator(MultipleLocator(1))
+        ax.xaxis.set_major_locator(MultipleLocator(1))
+        ax.xaxis.set_minor_locator(MultipleLocator(0.2))
 
-        plt.savefig(f"RDF3D_{'_'.join(F)}.png")
-        print(f"Image file: RDF3D_{'_'.join(F)}.png")
+        plt.savefig(f"PDF_{'_'.join(F)}.png")
+        print(f"Image file: PDF_{'_'.join(F)}.png")
 
     else:
         print('Must choose between one plot (-one) or multiple plots (-multi).')
         exit()
 
     print('Job done!')
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
